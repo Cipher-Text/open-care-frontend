9b5a2357d7dea97b39e7ce7861ea494e70b1e473
Habib2959 <rahman.habibjr59@gmail.com>
2025-06-19 12:00:18 +0600

    feat: initialize Next.js application with basic layout and home page
    - Created RootLayout component in layout.tsx to define the main structure of the app.
    - Added metadata for the application including title and description.
    - Implemented a simple home page in page.tsx with a welcome message.
    - Configured next.config.mjs for SPA output and custom build directory.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const config = {
-  apiUrl: import.meta.env.VITE_API_URL || "http://46.102.157.211:6700/",
-  itemsPerPage: parseInt(import.meta.env.VITE_ITEMS_PER_PAGE || "10", 10),
+	apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://46.102.157.211:6700/",
+	itemsPerPage: parseInt(process.env.NEXT_PUBLIC_ITEMS_PER_PAGE || "10", 10),
 };
 
 export default config;
