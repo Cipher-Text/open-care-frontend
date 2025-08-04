abc960b2e38955938425f0dfbe2b5b8ad92fceee
Sadman Sobhan <imran110219@gmail.com>
2025-08-04 14:52:52 +0600

    add

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	images: {
-		domains: ["http://46.102.157.211:6700", "api.opencarebd.com"],
-		unoptimized: true,
-	},
-};
-
-export default nextConfig;
--- src/config/index.ts
+++
@@ -1,6 +0,0 @@
-const config = {
-	apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://46.102.157.211:6700/",
-	itemsPerPage: parseInt(process.env.NEXT_PUBLIC_ITEMS_PER_PAGE || "10", 10),
-};
-
-export default config;
