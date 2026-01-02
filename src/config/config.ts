af2b9a0d0a158f683ef4b873926fabc09c7813bd
Sadman Sobhan <imran110219@gmail.com>
2026-01-02 22:41:05 +0600

    add

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,6 @@
-export const baseUrl = "https://api.opencarebd.com/api";
+const envBaseUrl =
+	process.env.NEXT_PUBLIC_API_BASE_URL || process.env.NEXT_PUBLIC_API_URL;
+
+export const baseUrl =
+	(envBaseUrl ? envBaseUrl.replace(/\/$/, "") : null) ||
+	"https://api.opencarebd.com/api";
